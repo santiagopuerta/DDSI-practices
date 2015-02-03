769f9c30f1f2e7f6f8f7c9f310fd44b9b158b2eb
Santiago Puerta <elwarxy@gmail.com>
2015-02-03 16:17:16 +0100

    Preparado para insertar un curso

--- inc/addCurso.php
+++ inc/addCurso.php
@@ -1,2 +1,22 @@
 <?php
     require ('inc/conexion.inc.php');
+
+    // Recogemos el contenido enviado desde formulario.
+    $nCurso = isset($_POST['nCurso']) ? $_POST['nCurso'] : '';
+    $numHora = isset($_POST['numHora']) ? $_POST['numHora'] : '';
+    $capacidadCurso = isset($_POST['capacidadCurso']) ? $_POST['capacidadCurso'] : '';
+
+    // Abrimos base de datos con funcion abrirDB de conexion.inc.php
+    $db = openDB();
+
+    // Preparamos la consulta.
+    $stmt = $db->prepare('INSERT INTO curso(nCurso, numHora, capacidadCurso) VALUES(?,?,?)');
+    $stmt->bind_param('sss',$nCurso, $numHora, $capacidadCurso);
+    // Ejecutamos la consulta.
+    $stmt->execute();
+
+    // Cerramos la conexion con la base de datos
+    $stmt->close();
+    $db->close();
+
+?>
