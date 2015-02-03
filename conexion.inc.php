3d28ffc47367ca587c8ac120041f8e5d64cf46e6
Santiago Puerta <elwarxy@gmail.com>
2015-02-03 11:59:20 +0100

    Comments code

--- conexion.inc.php
+++ conexion.inc.php
@@ -2,9 +2,9 @@
 
 require_once('configuracion.inc.php');
 
-// Funcion para primera conexion.
+// First conection
 function abrirDB(){
-    // conexion a la base de datos mediante mysqli
+    // Connect to the database using mysqli
     $db_server = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if ($db_server->connect_error) {
        die('Error de conexion (' . $db_server->connect_errno . ') '
