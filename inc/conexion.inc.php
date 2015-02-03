91930c05ab0c2f27a66552cdf4c4002a87709d2d
Santiago Puerta <elwarxy@gmail.com>
2015-02-03 15:59:26 +0100

    Formulario para crear un nuevo curso

--- inc/conexion.inc.php
+++ inc/conexion.inc.php
@@ -3,7 +3,7 @@
 require_once('configuracion.inc.php');
 
 // First conection
-function abrirDB(){
+function openDB(){
     // Connect to the database using mysqli
     $db_server = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if ($db_server->connect_error) {
