9336d946e0381bcce997cb07447594e4fd5e098b
Santiago Puerta <elwarxy@gmail.com>
2015-02-03 17:01:07 +0100

    Eliminado metodo para conectar con la base de datos. No funciona mysqli en 000webhost.

--- inc/addCurso.php
+++ inc/addCurso.php
@@ -1,22 +1,15 @@
 <?php
-    require ('inc/conexion.inc.php');
+    require_once('config.inc.php');
 
     // Recogemos el contenido enviado desde formulario.
     $nCurso = isset($_POST['nCurso']) ? $_POST['nCurso'] : '';
     $numHora = isset($_POST['numHora']) ? $_POST['numHora'] : '';
     $capacidadCurso = isset($_POST['capacidadCurso']) ? $_POST['capacidadCurso'] : '';
 
-    // Abrimos base de datos con funcion abrirDB de conexion.inc.php
-    $db = openDB();
+    // Conectamos con la base de datos
+    $host = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+    mysql_select_db(DB_NAME, $host);
 
-    // Preparamos la consulta.
-    $stmt = $db->prepare('INSERT INTO curso(nCurso, numHora, capacidadCurso) VALUES(?,?,?)');
-    $stmt->bind_param('sss',$nCurso, $numHora, $capacidadCurso);
-    // Ejecutamos la consulta.
-    $stmt->execute();
-
-    // Cerramos la conexion con la base de datos
-    $stmt->close();
-    $db->close();
-
+    // Insertamos en la base de datos.
+    mysql_query("INSERT INTO Curso (nCurso, numHora, capacidad) VALUES ('$nCurso','$numHora','$capacidadCurso')");
 ?>
